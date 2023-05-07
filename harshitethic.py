f0049849371048b5ca805ff85748db2bce5da4ce
readopedia <130125658+readopedia@users.noreply.github.com>
2023-05-07 19:36:41 +0530

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "6052466534:AAGtWQf49HsGZZo3cbFNVg2zrwxHKyBLWeM"
 else:
     tgkey = tgenv
 print(tgenv)
